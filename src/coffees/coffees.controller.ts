7e6ae34e657a900ff133d6b38a93b5de742ee8fe
melerog-daniel <melerog.daniel@gmail.com>
2023-12-30 18:46:29 +0100

    creating a basic service

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -8,37 +8,39 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
+  constructor(private readonly coffeesService: CoffeesService) {}
+
   //@Get('flavors')
   @Get()
   findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. Limit: ${limit}, offset: ${offset}`;
+    // const { limit, offset } = paginationQuery;
+    return this.coffeesService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+    return this.coffeesService.findOne(id);
   }
 
   @Post()
   //   @HttpCode(HttpStatus.GONE)
   create(@Body() body) {
-    return body;
-    // return `This action creates a coffee`;
+    return this.coffeesService.create(body);
   }
 
   // PUT http method update the entire object
   // PATCH http method update the part of the object that we want
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    return this.coffeesService.update(id, body);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string, @Body() body) {
-    return `This action removes #${id} coffee`;
+  remove(@Param('id') id: string) {
+    return this.coffeesService.remove(id);
   }
 }
